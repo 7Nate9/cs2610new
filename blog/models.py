d3d070c0191b632fec03ad628ee9682ee44dafb2
Nate Lovell <nate.lovell@aggiemail.usu.edu>
2017-11-22 00:25:20 +0000

    Created blog app. Migrated relevant files from HW4. Created base.html, archive.html, detail.html. Edited pages to extend base.html. Created models. Created urls.py, haven't yet put appropriate content in urls.py.

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+import datetime
+
+from django.utils import timezone
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    auth_name = models.CharField(max_length=50)
+    content = models.CharField(max_length=25000)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog)
+    nickname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    content = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
