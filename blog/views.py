fedfda6e2518cd1368a486a2eabf0cb00395a921
Nate Lovell <nate.lovell@aggiemail.usu.edu>
2017-11-22 01:56:53 +0000

    I've fixed up the relative links in the header, changed some styling, added the form to the detail view, and added a few blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,29 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponseRedirect
+from .models import Blog, Comment
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from django.shortcuts import render
+class HomeView(generic.ListView):
+    template_name='blog/home.html'
+    context_object_name='latest_blog_list'
+    
+    def get_queryset(self):
+        return Blog.objects.order_by('-pub_date')[:3]
 
-# Create your views here.
+class ArchiveView(generic.ListView):
+    template_name='blog/archive.html'
+    context_object_name='blog_list'
+    
+    def get_queryset(self):
+        return Blog.objects.order_by('-pub_date')
+        
+class DetailView(generic.DetailView):
+    model=Blog
+    template_name='blog/detail.html'
+    
+def tech(request):
+    return render(request, 'blog/tech.html')
+    
+def about(request):
+    return render(request, 'blog/about.html')
